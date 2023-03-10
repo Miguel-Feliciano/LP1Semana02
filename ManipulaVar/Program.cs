ab9b49fbd5ad2989247ec1650eec3ca72c79da74
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-10 11:55:58 +0000

    Exercise 1 solved

---
+++ ManipulaVar/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ManipulaVar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int x = int.Parse(args[0]);
+            int y = x;
+
+            int z = 2, v;
+            v = z + x++;
+        
+            Console.WriteLine(x);
+
+            int m = 3, n;
+            n = m + --y;
+            Console.WriteLine(y);     
+        }
+    }
+}
