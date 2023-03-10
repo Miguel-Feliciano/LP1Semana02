ddd16350912bdf5d051a04df6dc4a91150cc6224
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-10 12:12:53 +0000

    Exercise 4 volume calculation

---
+++ PerguntaValores/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PerguntaValores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double r;
+            double v;
+            double s;
+
+            Console.WriteLine("Enter height: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter radius: ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            v = Math.PI * Math.Pow(r ,2) * a;
+            Console.WriteLine($"The volume of the cylinder is: {v}");
+
+        }
+    }
+}
