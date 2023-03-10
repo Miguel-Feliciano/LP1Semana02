641f333450f414a7119ad658d609b9cd88a7aeea
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-10 12:20:56 +0000

    Exercise 5 solved

--- PerguntaValores/Program.cs
+++ PerguntaValores/Program.cs
@@ -10,17 +10,16 @@ namespace PerguntaValores
             double r;
             double v;
             double s;
-            double p = 3.1415926;
 
             Console.WriteLine("Enter height: ");
             a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Enter radius: ");
             r = Convert.ToDouble(Console.ReadLine());
 
-            v = p * (r * r) * a;
+            v = Math.PI * Math.Pow(r ,2) * a;
             Console.WriteLine($"The volume of the cylinder is: {v}");
 
-            s = (2 * p * r *(r + a));
+            s = (2 * Math.PI * r *(r + a));
             Console.WriteLine($"The surface area of the cylinder is: {s}");
 
         }
