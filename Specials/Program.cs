0a249df9255bd40bb544ddf8cabeb65d1ec9bf32
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-10 12:31:32 +0000

    Added and printed all values of the known types in exercise 6

---
+++ Specials/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Specials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"int - max: {int.MaxValue} | min: {int.MinValue}");
+            Console.WriteLine($"uint -max: {uint.MaxValue} | min: {uint.MinValue}");
+            Console.WriteLine($"byte -max: {byte.MaxValue} | min: {byte.MinValue}");
+            Console.WriteLine($"sbyte -max: {sbyte.MaxValue} | min: {sbyte.MinValue}");
+            Console.WriteLine($"short -max: {short.MaxValue} | min: {short.MinValue}");
+            Console.WriteLine($"ushort -max: {ushort.MaxValue} | min: {ushort.MinValue}");
+            Console.WriteLine($"long -max: {long.MaxValue} | min: {long.MinValue}");
+            Console.WriteLine($"ulong -max: {ulong.MaxValue} | min: {ulong.MinValue}");
+            Console.WriteLine($"float -max: {float.MaxValue} | min: {float.MinValue}");
+            Console.WriteLine($"double -max: {double.MaxValue} | min: {double.MinValue}");
+        }
+    }
+}
