f3f383d946be376df8e20c348aea62c71922cf28
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-12 15:37:02 +0000

    completed exercise 9

---
+++ ChatGPTMother/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ChatGPTMother
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool isExit = false;
+
+            while (!isExit)
+            {
+                Console.WriteLine("Ask me a question: ");
+                string question = Console.ReadLine();
+
+                switch (question.ToLower())
+                {
+                    case "what is your name?":
+                        Console.WriteLine("My name is ChatGPTMother.");
+                        break;
+                    case "what is the weather like today?":
+                        Console.WriteLine("I don't know.");
+                        break;
+                    case "what time is it?":
+                        Console.WriteLine("Its the time that it tells in your clock.");
+                        break;
+                    case "what is the meaning of life?":
+                        Console.WriteLine("Would'nt you like to know...");
+                        break;
+                    case "exit":
+                        isExit = true;
+                        break;
+                    default:
+                        Console.WriteLine("I'm sorry, I don't know the answer to that question.");
+                        break;
+                }
+            }
+
+
+
+        }
+    }
+}
