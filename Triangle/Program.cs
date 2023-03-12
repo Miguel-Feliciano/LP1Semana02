8d315a91286291ea37b59a1088704f6ffe877ae0
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-12 15:49:43 +0000

    completed exercise 10

---
+++ Triangle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("no good");
+                return;
+            }
+
+            int numberOfLines = int.Parse(args[0]);
+            char character = char.Parse(args[1]);
+
+            for (int i = 1; i <= numberOfLines; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(character);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
