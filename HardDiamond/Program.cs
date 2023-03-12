c0fd6cc55094975aeab707dd75bb3f7d25f8b226
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-12 15:25:22 +0000

    added class exercise

---
+++ HardDiamond/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HardDiamond
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int j;
+            int r;
+
+            Console.WriteLine("Please enter an odd number: ");
+            i = Convert.ToInt32( Console.ReadLine());
+            if (i % 2 == 0)
+            {
+                Console.WriteLine("I only want odd numbers!");
+            }
+            else
+            {
+                r = (i + 1) / 2;
+                for (i = 0; i <= r; i++)
+                {
+                    for (j = 1; j <= r - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+                    for (j = 1; j <= 2 * i - 1; j++)
+                    {
+                        Console.Write(".");
+                    }
+
+                    Console.Write("\n");
+                }
+
+                for (i = r - 1; i >= 1; i--)
+                {
+                    for (j = 1; j <= r - i; j++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (j = 1; j <= 2 * i - 1; j++)
+                    {
+                        Console.Write(".");
+                    }
+
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
