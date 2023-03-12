d508c98d53f8645f3d558c21810e61feada61e27
Miguel Feliciano <miguelafonsocoelhofeliciano@gmail.com>
2023-03-12 15:23:02 +0000

    completed exercise 8

---
+++ GranpaPatternSwitch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GranpaPatternSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+            //sintaxe por padrões
+            string response = question switch 
+            {
+                "How are you?" => "I'm fine, thank you",
+                "What's your name?" => "Gran'pa",
+                "What's your mission?" => "Destroy mankind!",
+                _ => "You got me, I'm not THAT smart!",  
+            };
+            Console.WriteLine(response);
+
+            
+        }
+    }
+}
